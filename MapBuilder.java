6e9846f6a52f436df426aa9fd79f8eb6dcc2e389
kiyo-hiko <kiyohiko.nijiura@gmail.com>
2015-02-17 13:33:21 +0900

    First version of my MapBuilder

---
+++ MapBuilder.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MapBuilder<K, V> {
+    public Map<K, V> from(List<K> ks, List<V> vs) {
+        int ksize;
+        int vsize;
+        int size = (ksize = ks.size()) > (vsize = vs.size()) ? ksize : vsize;
+        Map<K, V> map = new HashMap(size);
+        for (int i = 0; i < size; i++) {
+            map.put(ks.get(i), vs.get(i));
+        }
+        return map;
+    }   
+}
