3c28ebe54547174236884352b5b0a23e56444d7a
kiyo-hiko <kiyohiko.nijiura@gmail.com>
2015-02-17 14:19:17 +0900

    Fixed size bug

--- MapBuilder.java
+++ MapBuilder.java
@@ -6,7 +6,7 @@ public class MapBuilder<K, V> {
     public Map<K, V> from(List<K> ks, List<V> vs) {
         int ksize;
         int vsize;
-        int size = (ksize = ks.size()) > (vsize = vs.size()) ? ksize : vsize;
+        int size = (ksize = ks.size()) < (vsize = vs.size()) ? ksize : vsize;
         Map<K, V> map = new HashMap(size);
         for (int i = 0; i < size; i++) {
             map.put(ks.get(i), vs.get(i));
